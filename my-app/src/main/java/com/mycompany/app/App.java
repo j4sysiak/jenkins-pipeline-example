d500b45608d430b59787640203bfb80e8f69493e
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-30 21:43:22 +0100

    30-01-2023 21:42

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,14 +2,10 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    
-    
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World  22!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World  22!");
     }
 }
